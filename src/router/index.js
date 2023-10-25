313b3e99ae73a6ee162a6494700a3d6c40b0b1a6
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-10-25 17:30:48 +0200

    links added to the header

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Presentation from '../views/Presentation.vue'
+import Projets from '../views/Projets.vue'
+import Formulaire from '../views/Formulaire.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      
+      name: 'Presentation',
+      component: Presentation,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      
+      path: '/projets',
+      name: 'Projets',
+      component: Projets,
+    },
+    {
+      path: '/form',
+      name:'form',
+      component: Formulaire,
     }
   ]
 })
