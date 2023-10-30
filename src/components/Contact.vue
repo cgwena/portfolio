d4b259e0d5b292413ed03ee42c846ba4d5ee0142
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-10-30 14:52:55 +0100

    affichage message mail envoyé

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,11 +2,13 @@
     <section id="contact">
         <h2>Contactez-moi !</h2>
         <Form />
+        
     </section>
 </template>
 
 <script setup>
 import Form from './Form.vue'
+
 
   components:{
    Form
--- src/components/Modale.vue
+++ src/components/Modale.vue
@@ -41,36 +41,6 @@ img {
     width: 50px;
 }
 
-.bloc-modale {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.overlay {
-    background: rgba(0, 0, 0, 0.5);
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-}
-
-.modale {
-    text-align: center;
-    position: fixed;
-    left: 20%;
-    top: 25%;
-    border-radius: 20px;
-    background: #F8ECC9;
-    width: 60%;
-    padding-bottom: 30px;
-}
 
 .contenu {
     display: flex;
