eae18e811ea300ed86f5a71dc65b2f8c5ce31812
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2023-11-12 16:20:00 +0100

    projects file created

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,6 +10,7 @@
 
 <script>
 import Modale from './Modale.vue'
+import {projects} from '../datas/projects'
 export default {
     name: 'Projets',
     components: { 
@@ -18,36 +19,7 @@ export default {
     data() {
         return {
             revele: false,
-            projects: [
-                {
-                    id: '1',
-                    title: 'Mon CV en Ligne', 
-                    picture: '/src/assets/pictures/MiniatureCV.png',
-                    alt: 'Une image de mon CV : le haut du CV',
-                    date: '9 octobre 2023',
-                    logos: ['/src/assets/icons/html-5.png', '/src/assets/icons/css-3.png'],
-                    gh_link: 'https://github.com/cgwena/Creer_un_CV',
-                    open_project: '/src/assets/projects/Creer_un_CV-1.png'
-                },
-                {
-                    id: '2',
-                    title: 'Un espace de commentaires',
-                    picture: '/src/assets/pictures/MiniatureCommentaires.png',
-                    alt: "une copie d'écran d'un espace de commentaires",
-                    date: '15 octobre 2023',
-                    logos: ['/src/assets/icons/html-5.png', '/src/assets/icons/css-3.png', '/src/assets/icons/js.png'],
-                    gh_link: 'https://github.com/cgwena/DynamiserUnEspaceDeCommentaires',
-                    open_project: '/src/assets/projects/DynamiserUnEspaceDeCommentaires-1.png'
-                },
-                {
-                    id: '3',
-                    title: 'Un cahier des charges', 
-                    picture: '/src/assets/pictures/MiniatureCahierDesCharges.png',
-                    alt: 'La page de garde du chaier des charges du site web "La socketterie"',
-                    date: '17 octobre 2023',
-                    open_project: '/src/assets/projects/Cahier_des_charges-La_Socketterie.pdf'
-                }
-            ],
+            projects: projects,
             selectedProject: null,
         }
     },
